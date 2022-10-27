c608e9730f46f7a65fe9c10267244be3751e31aa
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-10-27 09:58:33 +0300

    Set city to LS and get changed weather data from API

--- classes/UI.js
+++ classes/UI.js
@@ -3,13 +3,30 @@ class UI {
         this.description = document.querySelector(`#description`)
         this.temp = document.querySelector(`#temp`)
         this.city = document.querySelector(`#location`)
+        this.windSpeed = document.querySelector(`#windSpeed`)
+        this.windDeg = document.querySelector(`#windDeg`)
+        this.Humidity = document.querySelector(`#Humidity`)
+        this.pressure = document.querySelector(`#pressure`)
+        this.visibility = document.querySelector(`#visibility`)
     }
         drawWeather(data) {
         var celcius = Math.round(parseFloat(data.main.temp) -273.15);
         var description = data.weather[0].description;
+        let speed = data.wind.speed;
+        let deg = data.wind.deg;
+        let humidity = data.main.humidity;
+        let pressure = data.main.pressure;
+        let visibility = data.visibility;
+
 
         this.description.innerHTML = description;
+        this.windSpeed.innerHTML = speed + ` m/s`;
+        this.windDeg.innerHTML = deg + `&deg;`;
+        this.Humidity.innerHTML = `Humidity ` + humidity + `%`;
+        this.pressure.innerHTML = `Pressure ` + pressure + ` psi`;
         this.temp.innerHTML = celcius + `&deg;`;
+        this.visibility.innerHTML = `Visibility ` + visibility + ` m`;
+
         this.city.innerHTML = data.name;
     }
 }
